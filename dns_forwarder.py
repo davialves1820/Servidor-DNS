7731888a17bd41bee6e765315fc34565ae126441
Davi Alves <davialvesr18@gmail.com>
2025-09-13 14:20:18 -0300

    Importacoes

---
+++ dns_forwarder.py
@@ -0,0 +1,4 @@
+import socket # Permite enviar e receber pacotes UDP
+import struct # Para empacotar e desempacotar bytes, formato de mensagens DNS
+import threading # Permite atender múltiplos clientes simultaneamente, criando threads para cada requisição DNS]
+import dnslib # Biblioteca para facilitar a construção de pacotes DNS
